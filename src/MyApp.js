ea18d48141b7cdab1d23eaaf1f24c2f51d21cb92
Grant Holland <gwholland3@gmail.com>
2021-01-15 14:58:54 -0800

    Add function to make HTTP DELETE calls to the backend

--- src/MyApp.js
+++ src/MyApp.js
@@ -7,15 +7,30 @@ function MyApp() {
     const [characters, setCharacters] = useState([]);
 
     function removeOneCharacter (index) {
-        const updated = characters.filter((character, i) => {
-                return i !== index;
-                });
-        setCharacters(updated);
+        makeDeleteCall(index).then(result => {
+            if (result.data.status_code === 204) {
+                const updated = characters.filter((character, i) => {
+                        return i !== index;
+                        });
+                setCharacters(updated);
+            }
+        });
+    }
+
+    async function makeDeleteCall(index) {
+        try {
+            const response = await axios.delete('http://localhost:5000/users/' + characters[index].id);
+            return response;
+        }
+        catch (error) {
+            console.log(error);
+            return false;
+        }
     }
 
     function updateList(person) {
         makePostCall(person).then(result => {
-            if (result.data[0].status_code == 201)
+            if (result.data[0].status_code === 201)
                 setCharacters([...characters, result.data[1]]);
         });
     }
